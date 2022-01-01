43e6b58617ce0772313eb5c8374d6283c7507736
Juan Manuel Hernandez Duran <juan.manuel@accely.us>
2021-12-31 20:24:30 -0600

    Imagen nueva de carro

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -17,6 +17,35 @@ sap.ui.define(
           var oTableCustomer = this.getView().byId("idCustomerTable");
           oTableProducts.getBinding("items").refresh();
         },
+        onNewModel: function () {
+          var prod = [];
+
+          sap.ui.core.BusyIndicator.show();
+          var oModel = new sap.ui.model.odata.ODataModel(
+            "/V2/Northwind/Northwind.svc/"
+          );
+          oModel.read("/Products", {
+            success: function (res) {
+              console.log(res);
+              debugger;
+              for (var i = 0; i < res.results.length; i++) {
+                prod.push({
+                  ProductName: res.results[i].ProductName,
+                  ProductID: res.results[i].ProductID,
+                  SupplierID: res.results[i].SupplierID,
+                  UnitPrice: res.results[i].UnitPrice,
+                });
+              }
+
+              var data = new sap.ui.model.json.JSONModel(prod);
+              this.getView().setModel(data, "oModelMNA");
+              sap.ui.core.BusyIndicator.hide();
+            }.bind(this),
+            error: function (err) {
+              debugger;
+            },
+          });
+        },
       }
     );
   }
